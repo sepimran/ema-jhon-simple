e39df9253384dc90b3ff5ca6ee6c17266129a0f5
sepimran <imranhossain0461@gmail.com>
2023-04-03 12:54:59 +0600

    card added

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,23 +1,29 @@
 import React, { useEffect, useState } from 'react';
+import {addToDb, getShoppingCart} from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
     const [products , setProducts] = useState([]);
+    const [cart , setCart] = useState([]);
+
     useEffect( () => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
     },[]);
 
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        console.log(storedCart);
+    },[]);
 
-    const [cart , setCart] = useState([]);
     const handleAddToCart = (product) =>{
         let newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id);
 
-        
     }
 
 
